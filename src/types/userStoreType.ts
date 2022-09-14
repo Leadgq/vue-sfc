9bb22245562dbf255c8e8c394e0bc48abb4dffc5
Leadgq <2643336540@qq.com>
2022-09-14 11:49:58 +0800

    注释添加

--- src/types/userStoreType.ts
+++ src/types/userStoreType.ts
@@ -1,12 +1,15 @@
+// 用户响应
 export type userInfoResponseType = {
     name: number | string | undefined;
     age: number | string | undefined;
     token: string;
     id: number | string | undefined;
 }
+// 请求登录参数
 export type userRequestType = {
     id: number | string
 }
+// 请求注册参数
 export type registerUserType = {
     name: string,
     id: number | number
