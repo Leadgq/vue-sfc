924df34a2729f6108ef3ab8b14e7c1f966c50497
gaoqiang <gaoqiang@yundun119.com>
2022-09-28 14:34:41 +0800

    [修改]打包出错

--- src/components/asyncChildren.vue
+++ src/components/asyncChildren.vue
@@ -1,19 +1,29 @@
-<script setup lang="ts">
-const counter = ref(1)
-const doubled = computed(() => counter.value * 2)
-watch(doubled, () => console.log(doubled.value))
-watchEffect(() => console.log("Count: ", doubled.value))
-counter.value = 2
-setTimeout(() => {
-  counter.value = 4
-})
+<script setup lang='ts'>
+interface UseCounterOptions {
+  min: number
+  max: number
+}
+
+function useCounter(initialValue = 0, options: UseCounterOptions) {
+  const {min, max} = options;
+  let count = ref(initialValue);
+  const inc = () => count.value = Math.min(unref(count) + 1, max);
+  const dec = () => count.value = Math.max(unref(count) - 1, min);
+  const reset = () => count.value = initialValue;
+  return {
+    inc,
+    count,
+    reset,
+    dec
+  }
+}
+
+const {count, inc, dec, reset} = useCounter(0, {min: 0, max: 10})
 </script>
 
 <template>
-  <div>
-    <p>
-      {{ doubled }}
-    </p>
-  </div>
+  <p>Count: {{ count }}</p>
+  <el-button @click="inc">inc</el-button>
+  <el-button @click="dec">dec</el-button>
+  <el-button @click="reset">reset</el-button>
 </template>
-
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,17 @@
 import {createApp} from 'vue';
 import App from './App.vue';
 import {setStore} from "@/store";
-import router, {setRouter} from "@/router/index";
+import {setRouter} from "@/router";
 import {loadTailWind} from "@/plugin/tailwind";
 import {loadElementStyle} from "@/plugin/loadElement";
 
-router.isReady().then(() => {
-    const app = createApp(App);
-    // 引入TailWind
-    loadTailWind();
-    // 引入非组件api的样式
-    loadElementStyle();
-    // 引入pinia
-    setStore(app);
-    // 引入router
-    setRouter(app);
-    app.mount('#app')
-})
+const app = createApp(App);
+// 引入TailWind
+loadTailWind();
+// 引入非组件api的样式
+loadElementStyle();
+// 引入pinia
+setStore(app);
+// 引入router
+setRouter(app);
+app.mount('#app')
