ae9fd5dffe446a5b1523c3fe615d17c51ff70cb7
gaoqiang <gaoqiang@yundun119.com>
2022-10-10 08:30:58 +0800

    记住密码

--- src/views/login.vue
+++ src/views/login.vue
@@ -1,91 +1,136 @@
 <template>
-  <div class="login">
-    <el-form :model="loginData" label-width="120px" :rules="loginRules" ref="ruleFormRef">
-      <el-form-item label="用户名:" prop="name">
-        <el-input v-model.trim="loginData.name"/>
-      </el-form-item>
-      <el-form-item label="密码:" prop="password">
-        <el-input v-model.trim="loginData.password" type="password"/>
-      </el-form-item>
-      <el-form-item label="手机号:" prop="phone">
-        <el-input v-model.trim="loginData.phone" type="text" :maxlength="11"/>
-      </el-form-item>
-      <el-form-item>
-        <el-button type="primary" @click="loginUser">登录</el-button>
-        <el-checkbox v-model="checkState" label="记住密码" size="large" class="ml-5"/>
-      </el-form-item>
-    </el-form>
+  <div class="login-container">
+    <div class="login-content">
+      <div class="container-left">
+        <img :src="bgImg" alt="" />
+      </div>
+      <div class="container-right">
+        <el-form :model="loginData" label-width="90px" :rules="loginRules" ref="ruleFormRef">
+          <el-form-item label="用户名:" prop="name">
+            <el-input v-model.trim="loginData.name" />
+          </el-form-item>
+          <el-form-item label="密码:" prop="password">
+            <el-input v-model.trim="loginData.password" type="password" />
+          </el-form-item>
+          <el-form-item label="手机号:" prop="phone">
+            <el-input v-model.trim="loginData.phone" type="text" :maxlength="11" />
+          </el-form-item>
+          <el-form-item class="login-action">
+            <el-button type="primary" @click="loginUser" class="login-btn">登录</el-button>
+            <el-checkbox v-model="checkState" label="记住密码" size="large" class="ml-8" />
+          </el-form-item>
+        </el-form>
+      </div>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import {loginHook} from "@/tools/hook/hook";
-import type {FormInstance, FormRules} from 'element-plus'
-import {isAvailableObject, isAvailablePhone, setCookie} from "@/tools/lib";
+import { loginHook } from "@/tools/hook/hook";
+import type { FormInstance, FormRules } from "element-plus";
+import { getCookie, isAvailableObject, isAvailablePhone, setCookie } from "@/tools/lib";
 import userStore from "@/store/userStore";
-import {encode} from 'js-base64';
+import { encode, decode } from "js-base64";
+import bgImg from "@/assets/bg.jpg";
+
+interface loginType {
+  name: string;
+  password: string;
+  phone: string;
+}
 
 const storeInstance = userStore();
 // 检查手机号
 const checkPhone = (rule: any, value: any, callback: any) => {
   if (!isAvailablePhone(value)) {
-    return callback(new Error('检查手机号是否正确'))
+    return callback(new Error("检查手机号是否正确"));
   } else {
     callback();
   }
-}
+};
 // 规则
 let loginRules = reactive<FormRules>({
-  name: [
-    {required: true, message: '请输入用户名', trigger: 'blur'},
-  ],
+  name: [{ required: true, message: "请输入用户名", trigger: "blur" }],
   password: [
-    {required: true, message: '请输入密码', trigger: 'blur'},
-    {min: 6, max: 15, message: '密码长度6到15位', trigger: 'blur'},
+    { required: true, message: "请输入密码", trigger: "blur" },
+    { min: 6, max: 15, message: "密码长度6到15位", trigger: "blur" },
   ],
   phone: [
-    {required: true, message: '请输入手机号', trigger: 'blur'},
-    {validator: checkPhone, trigger: 'blur'}
+    { required: true, message: "请输入手机号", trigger: "blur" },
+    { validator: checkPhone, trigger: "blur" },
   ],
 });
-let loginData = reactive({
-  name: '',
-  password: '',
-  phone: ''
-})
+let loginData = reactive<loginType>({
+  name: "",
+  password: "",
+  phone: "",
+});
 let checkState = ref(false);
 const ruleFormRef = ref<FormInstance>();
-const {login} = loginHook();
+const { login } = loginHook();
 const loginUser = () => {
   login(loginData, unref(ruleFormRef)!);
   modifyState();
-}
+};
 onMounted(() => {
-  handlerRememberPasswordState()
-})
+  handlerRememberPasswordState();
+});
 // 处理记住密码操作
 const handlerRememberPasswordState = () => {
   checkState.value = storeInstance.rememberPasswordState;
   if (checkState.value) {
-    console.log(encode(window.location.origin))
+    // cookie解密
+    const cookiesValues = getCookie(encode(window.location.origin));
+    if (cookiesValues) {
+      const formValue = JSON.parse(decode(cookiesValues)) as loginType;
+      Object.assign(loginData, formValue);
+    }
   }
-}
+};
 // 修改记住密码状态
 const modifyState = () => {
   storeInstance.modifyRememberPasswordState(checkState.value);
   if (checkState.value && isAvailableObject(loginData)) {
-    setCookie(encode(`${window.location.origin}`), encode(JSON.stringify(loginData)), 1000)
+    setCookie(encode(`${window.location.origin}`), encode(JSON.stringify(loginData)), 1000);
   }
-}
+};
 </script>
 
 <script lang="ts">
 export default {
-  name: 'login'
-}
+  name: "login",
+};
 </script>
 <style lang="scss" scoped>
-.login {
-  @apply w-[400px];
+.login-container {
+  @apply h-full bg-violet-200 flex flex-col items-center justify-center;
+
+  .login-content {
+    @apply flex bg-white w-[800px] h-[400px] rounded shadow-md items-center;
+
+    .container-left {
+      @apply w-[400px] h-full;
+
+      img {
+        @apply w-full h-full object-cover;
+      }
+    }
+
+    .container-right {
+      @apply w-[380px];
+
+      &:deep(.el-form-item) {
+        @apply mb-7;
+      }
+
+      .login-action {
+        @apply mb-0;
+
+        .login-btn {
+          @apply ml-10;
+        }
+      }
+    }
+  }
 }
 </style>
