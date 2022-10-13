1f583fd6f0039c8798473dcb9349b883066c3cd6
gaoqiang <gaoqiang@yundun119.com>
2022-10-13 15:47:30 +0800

    [修改] 删除测试用例

--- src/components/treeComponent/treeComponentTest.vue
+++ src/components/treeComponent/treeComponentTest.vue
@@ -8,13 +8,11 @@
 import TreeChildren from "@/components/treeComponent/treeChildren.vue";
 import {loadFile} from "@/tools/lib";
 import {TreeData} from "@/types/tree";
-import {flattenArray} from "@/tools/tree/treeLib";
 
 
 let treeDataArray = ref<TreeData[]>([]);
 const load = async () => {
   treeDataArray.value = (await loadFile("treeData")) as TreeData[];
-  console.log(flattenArray(treeDataArray.value!, false));
 };
 onMounted(() => {
   load();
