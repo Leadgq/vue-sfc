df90644ff1c226f5fbc0d534e37b3ca955e91d01
gaoqiang <gaoqiang@yundun119.com>
2022-10-14 09:19:13 +0800

    [修改] 异步组件提供

--- src/components/treeComponent/treeComponentTest.vue
+++ src/components/treeComponent/treeComponentTest.vue
@@ -1,19 +1,23 @@
 <template>
   <div>
-    <TreeChildren :data="treeDataArray" v-model:testValue="testValue"/>
+    <TreeChildren :data="treeDataArray" v-model:testValue="testValue" v-if="flag"/>
     <div>{{ testValue }}</div>
+    <el-button @click="flag= !flag">显示组件</el-button>
   </div>
 </template>
 
 <script setup lang="ts">
-import TreeChildren from "@/components/treeComponent/treeChildren.vue";
 import {loadFile} from "@/tools/lib";
 import {TreeData} from "@/types/tree";
 
-
+const TreeChildren = defineAsyncComponent({
+  loader: () => import('@/components/treeComponent/treeChildren.vue'),
+  delay: 5000,
+  timeout: 3000
+})
+let flag = ref(false);
 let treeDataArray = ref<TreeData[]>([]);
-
-let  testValue =  ref('0');
+let testValue = ref('父组件的值');
 const load = async () => {
   treeDataArray.value = (await loadFile("treeData")) as TreeData[];
 };
