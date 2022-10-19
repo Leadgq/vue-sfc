ccac479c2f2a81bd6c0431f9fa23d42b87eb9be6
gaoqiang <gaoqiang@yundun119.com>
2022-10-19 10:51:02 +0800

    [添加] blob方法 -  blob 转换为base64

--- src/tools/blob/blobLib.ts
+++ src/tools/blob/blobLib.ts
@@ -9,11 +9,12 @@ export const dataUrlToBlob = (base64: string, mimeType: string) => {
     return new Blob([ab], {type: mimeType});
 }
 // 图片压缩逻辑
-export const recursionCompressH5 = (blob: Blob, type: string) => {
+export const recursionCompressH5 = (blob: Blob, type: string): Promise<string> => {
     let img = document.createElement("img");
     let canvas = document.createElement("canvas");
     return new Promise((resolve) => {
-        img.src = URL.createObjectURL(blob);
+        const blobUrl = URL.createObjectURL(blob);
+        img.src = blobUrl;
         img.onload = () => {
             let screenWidth = img.width;
             let screenHeight = img.height;
@@ -24,6 +25,20 @@ export const recursionCompressH5 = (blob: Blob, type: string) => {
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
             let imageData = canvas.toDataURL(type, 0.1);
             resolve(imageData);
+            // 释放内存
+            URL.revokeObjectURL(blobUrl);
         }
+    })
+}
+//blob转换为base64
+export const bolbToBase64 = (blob: Blob): Promise<string | ArrayBuffer> => {
+    return new Promise((resolve) => {
+        const fileReader = new FileReader();
+        fileReader.onload = (event) => {
+            if (event.target?.result) {
+                resolve(event.target.result);
+            }
+        }
+        fileReader.readAsDataURL(blob);
     })
 }
