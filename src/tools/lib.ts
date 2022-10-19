35a5a0d289c85c0f81cc59be797b869d85698928
gaoqiang <gaoqiang@yundun119.com>
2022-10-19 16:30:53 +0800

    [添加] 工具方法新增

--- src/tools/lib.ts
+++ src/tools/lib.ts
@@ -33,12 +33,8 @@ export const isAvailablecarNo = (no: string | Ref<string>) => {
     const newCardReg = /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}(([0-9]{5}[DF]$)|([DF][A-HJ-NP-Z0-9][0-9]{4}$))/
     // 旧车牌
     const oldCrdReg = /^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-HJ-NP-Z0-9]{4}[A-HJ-NP-Z0-9挂学警港澳]{1}$/
-    if (cardNo.length === 7) {
-        return oldCrdReg.test(cardNo)
-    }
-    if (cardNo.length === 8) {
-        return newCardReg.test(cardNo)
-    }
+    if (cardNo.length === 7) return oldCrdReg.test(cardNo)
+    if (cardNo.length === 8) return newCardReg.test(cardNo)
     return false
 }
 //只能输入字母
