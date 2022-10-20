8090f26dd757d014f924131d83b41596c2d9e1cd
gaoqiang <gaoqiang@yundun119.com>
2022-10-20 10:23:55 +0800

    [修改] 图片压缩

--- src/tools/blob/blobLib.ts
+++ src/tools/blob/blobLib.ts
@@ -1,6 +1,6 @@
 // base64转换为blob，返回的纯blob流、
 // 提交后台需要URL.createObjectURL(blob)
-export const dataUrlToBlob = (base64: string, mimeType: string) => {
+export const dataUrlToBlob = (base64: string, mimeType: 'image/jpeg') => {
     let bytes = window.atob(base64.split(",")[1]);
     let ab = new ArrayBuffer(bytes.length);
     let ia = new Uint8Array(ab);
@@ -10,21 +10,19 @@ export const dataUrlToBlob = (base64: string, mimeType: string) => {
     return new Blob([ab], {type: mimeType});
 }
 // 图片压缩逻辑
-export const recursionCompressH5 = (blob: Blob, type: string): Promise<string> => {
+export const recursionCompressH5 = (blob: Blob, quality: number): Promise<string> => {
     let img = document.createElement("img");
     let canvas = document.createElement("canvas");
     return new Promise((resolve) => {
         const blobUrl = URL.createObjectURL(blob);
         img.src = blobUrl;
         img.onload = () => {
-            let screenWidth = img.width;
-            let screenHeight = img.height;
-            canvas.width = screenWidth;
-            canvas.height = screenHeight;
+            canvas.width = img.width;
+            canvas.height = img.height;
             let ctx = canvas.getContext("2d")!;
             ctx.clearRect(0, 0, canvas.width, canvas.height); // 清除画布
             ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
-            let imageData = canvas.toDataURL(type, 0.1);
+            let imageData = canvas.toDataURL('image/jpeg', quality);
             resolve(imageData);
             // 释放内存
             URL.revokeObjectURL(blobUrl);
