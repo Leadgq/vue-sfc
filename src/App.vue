3a0084849825dde2a9160f71d798cb22f4a6556b
gaoqiang <gaoqiang@yundun119.com>
2022-11-18 14:43:21 +0800

    [添加] 新增日期格式化

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,10 @@
 <template>
-  <el-config-provider :locale="locale">
+  <el-config-provider :locale="zhCn">
     <router-view></router-view>
   </el-config-provider>
 </template>
 <script setup lang="ts">
-import zhCn from "element-plus/lib/locale/lang/zh-cn";
-
-const locale = zhCn;
-</script>
+import zhCn from "element-plus/lib/locale/lang/zh-cn";</script>
 <style lang="scss">
 html,
 body {
