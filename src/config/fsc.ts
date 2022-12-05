edf644140cf690c0d206d47c8f4943722e6c3fb8
gaoqiang <gaoqiang@yundun119.com>
2022-12-05 14:11:48 +0800

    [添加] [描述]

--- src/config/fsc.ts
+++ src/config/fsc.ts
@@ -39,6 +39,6 @@ export const sfcConfigList: sfc[] = [
   {
     name: "自动更新",
     componentName: "Modifier",
-    desc: "测试组件的v-model:value、自动更新外界的值、修饰符(这类似于.trim .lazy)"
+    desc: "测试组件的v-model:value、自动更新外界的值、修饰符(这类似于.trim .lazy)、使用withDefaults提供默认值(这个方式支持ts)"
   }
 ];
